b425a6de87b77ca5af71461da5f97869b61d08db
vivianpppp <chunying_su@hotmail.com>
2020-04-09 00:28:59 +0000

    Updating all questions

--- invent-code-1/main.cpp
+++ invent-code-1/main.cpp
@@ -1,9 +1,18 @@
-#incldue <iostream>
+#include <iostream>
 #include <string>
 using namespace std;
 
 int main() {
+  int original;
+  float original_float;
+  original_float = 13.5;
 
+  cout << "Please enter an integer: ";
+  cin >> original;
+
+  int clone = original + 14;
+  int product = original * clone;
+  cout << "The product is: " << product << endl;
 }
 
 /*
@@ -11,5 +20,8 @@ Table at the end of the program.
 
 Type   | Name     | Value
 ------ | -------- | -----
-       |          |
+int    | original | if = 2
+float  | original_float| 13.5
+int    | clone    | 16
+int    | product  | 32
 */
