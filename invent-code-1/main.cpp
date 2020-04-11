38275f6ab5005c71b007dd4153d46fe063f0c171
nicoleyip1876 <nicoleyip1876@yahoo.com>
2020-04-11 04:29:18 +0000

    Updating all questions

--- invent-code-1/main.cpp
+++ invent-code-1/main.cpp
@@ -5,11 +5,12 @@ using namespace std;
 int main() {
   int original;
   //float original_float = 13.5;
+  //According to the slack thread with vaibahv, we should not create original_float variable. there's an error in this lesson.
   cout << "Please enter an integer ";
   cin >> original;
 
   int clone = original + 14;
-  int product = original + clone;
+  int product = original * clone;
   cout << "The product is: " << product << endl;
 
 }
@@ -23,5 +24,5 @@ Type   | Name     | Value
    
   int   original   1
   int   clone      15
-  int   product    16
+  int   product    15
 */
