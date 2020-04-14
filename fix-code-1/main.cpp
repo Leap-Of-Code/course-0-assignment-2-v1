d9340637a342dac6875755b91134d4c3c6b9694b
jinghe315 <jinghe315@yahoo.com>
2020-04-14 02:45:22 +0000

    Updating all questions

--- fix-code-1/main.cpp
+++ fix-code-1/main.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 int main() {
   float rain_this_year = 321.5;
-  float rain_december = 67;
-  string yearly_rain_less_december = rain_this_year - rain_december;
+  float rain_december = 67.0;
+  
+  float yearly_rain_less_december = rain_this_year - rain_december;
   cout << "The amount of rain from January to November was: " << yearly_rain_less_december << endl;
 }
--- invent-code-1/main.cpp
+++ invent-code-1/main.cpp
@@ -1,15 +1,32 @@
-#incldue <iostream>
+#include <iostream>
 #include <string>
 using namespace std;
 
 int main() {
+ int original;
+  float original_float;
+  original_float = 13.5;
+
+  cout << "please enter an integer:"; 
+  cin >> original;
+
+  int clone = original + 14; 
+  int product = original * clone;
+
+  cout << "The product is:" << product << endl; 
 
 }
 
 /*
 Table at the end of the program.
 
-Type   | Name     | Value
------- | -------- | -----
-       |          |
+Type   | Name           | Value
+------ | --------       | -----
+int    | original       | 5
+float  | original_float | 13.5
+int    | clone          | 19
+int    | product        | 95
+
 */
+
+
