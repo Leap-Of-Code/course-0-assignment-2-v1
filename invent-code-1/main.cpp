d503cbeb7d2b337074cfc4665108b274444a2c11
cirumple <17078803+cirumple@users.noreply.github.com>
2020-04-22 00:05:24 +0000

    Updating all questions

--- invent-code-1/main.cpp
+++ invent-code-1/main.cpp
@@ -1,9 +1,18 @@
-#incldue <iostream>
+#include <iostream>
 #include <string>
 using namespace std;
 
 int main() {
+  int original = 0;
 
+  float original_float ; 
+  original_float = 13.5; 
+  std:: cout << "Please enter an integer "; 
+  std::cin >> original;
+  std::cout << endl;  
+  const int clone_original = original + 14; 
+  const int product = original * clone_original;  
+  std::cout << "The product is " << product << endl; 
 }
 
 /*
