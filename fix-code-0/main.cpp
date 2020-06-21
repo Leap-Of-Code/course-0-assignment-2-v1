5e92f3c9111620af5d3c2fde7777419179ae41ec
Harshelite2503 <67180856+Harshelite2503@users.noreply.github.com>
2020-06-21 12:45:54 +0000

    Updating all questions

--- fix-code-0/main.cpp
+++ fix-code-0/main.cpp
@@ -6,8 +6,8 @@ int main() {
   string cat_food_price = "Super special cat food costs 20 per pound.";
   int cat_food_cost = 20;
   int cat_food_amount_lbs = 30;
-  int cat_food_price = cat_food_cost * cat_food_amount;
+  int cat_food_price1 = cat_food_cost * cat_food_amount_lbs;
   cout << cat_food_price;
   cout << " Therefore with " << cat_food_amount_lbs << " pounds, ";
-  cout << "the total cost will be " << cat_food_price << endl;
+  cout << "the total cost will be " << cat_food_price1 << endl;
 }
